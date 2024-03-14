a4dfe6ff7b2f4d17267b49a146a825b398dc3aec
rutulkakadiya <rutulkakadiya973@gmail.com>
2024-03-14 15:11:33 +0530

    main

--- Project 2 Momentum/3.c
+++ Project 2 Momentum/3.c
@@ -1,15 +1,3 @@
-// Write a Program to input electricity unit charges and calculate the total electricity bill according to the given condition:
-// For the first 50 units Rs. 0.50/unit
-// For the next 100 units Rs. 0.75/unit
-// For the next 100 units Rs. 1.20/unit
-// For units above 250 Rs. 1.50/unit
-// An additional surcharge of 20% is added to the bill.
-// For example,
-// Input:
-// Enter electricity units: 435
-
-// Output: 
-// Electricity Bill = Rs. 597.00
 #include <stdio.h>
 
 void main()
