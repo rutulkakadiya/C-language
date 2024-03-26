6eb0fa8ae281714a48a958ee489ffc55946f9ebb
rutulkakadiya <rutulkakadiya973@gmail.com>
2024-03-26 14:53:27 +0530

    Project

---
+++ Day 15 Circlet/3.c
@@ -0,0 +1,44 @@
+// Write a Program to print the below pattern using nested for loop.
+//         5
+//       4 5
+//     3 4 5
+//   2 3 4 5
+// 1 2 3 4 5
+
+#include<stdio.h>
+
+main(){
+    for(int i=5; i>=1; i--){
+
+        for(int k=i; k>=2; k--){
+            printf(" ");
+        }
+
+        for(int j=i; j<=5; j++){
+            printf("%d",j);
+        }
+
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
