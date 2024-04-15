f60a647ae0a401c654ba67745c41e195a3e8dafc
rutulkakadiya <rutulkakadiya973@gmail.com>
2024-04-15 15:43:40 +0530

    Pointer

--- Day 22 Pointer/1.c
+++ Day 22 Pointer/1.c
@@ -1,24 +1,42 @@
-// Write a Program to find MAXIMUM between 2 numbers in TSRN.
+// Write a Program to find the reverse of a 1D array using a Chain of Pointers.
+// For example,
+// Input:
+// Enter the array's size: 5
 
-#include<stdio.h>
+// Enter array elements:
+// a[0] = 5
+// a[1] = 9
+// a[2] = 4
+// a[3] = 7
+// a[4] = 3
 
-max(int *n1, int *n2){
-    if(*n1 < *n2){
-        printf("Maximum =%d",*n2);
+// Output:
+// Reversed array elements:
+// 3, 7, 4, 9, 5
+
+#include <stdio.h>
+
+main()
+{
+
+    int a;
+    int *b;
+
+    printf("Enter the size of array=");
+    scanf("%d", &a);
+
+    int arr[a];
+
+    for (int i = 0; i < a; i++)
+    {
+        printf("Enter the value of array=");
+        scanf("%d", &arr[i]);
     }
-    else{
-        printf("Maximum =%d",*n1);
+
+    b = &arr;
+
+    for (int i = a - 1; i >= 0; i--)
+    {
+        printf("%d\n", *(b + i));
     }
-}
-
-main(){
-    int a,b;
-
-    printf("Enter the value of a=");
-    scanf("%d",&a);
-    
-    printf("Enter the value of b=");
-    scanf("%d",&b);
-
-    max(&a,&b);
 }
